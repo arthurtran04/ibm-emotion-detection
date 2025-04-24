8c339cea5079b2f2c43424154759ceae88054477
arthurtran04 <minhphat.workmail@gmail.com>
2025-04-24 04:21:31 -0400

    Update file

--- app.py
+++ app.py
@@ -11,7 +11,7 @@ def render_index_page():
 def emo_detector():
     text_to_analyze = request.args.get("textToAnalyze")
     response = emotion_detector(text_to_analyze)
-    anger_score, disgust_score, fear_score, joy_score, sadness_score, dominant_emotion = response
+    anger_score, disgust_score, fear_score, joy_score, sadness_score, dominant_emotion = response.values()
     if dominant_emotion is None:
         return 'Invalid text! Please try again!'
     return f"For the given statement, the system response is 'anger': {anger_score}, 'disgust': {disgust_score}, 'fear': {fear_score}, 'joy': {joy_score} and 'sadness': {sadness_score}. The dominant emotion is {dominant_emotion}."
